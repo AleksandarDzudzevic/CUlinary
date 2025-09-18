336ed5b8a25c187b63803af0416bf3900bf5d017
ad2364 <ad2364@cornell.edu>
2025-09-18 17:02:33 -0400

    .

--- add-simple-preferences-column.sql
+++
@@ -1,8 +0,0 @@
--- Add simple_preferences column to user_preferences table
-ALTER TABLE user_preferences 
-ADD COLUMN IF NOT EXISTS simple_preferences JSONB DEFAULT '{}';
-
--- Update existing rows to have empty object for the new column
-UPDATE user_preferences 
-SET simple_preferences = '{}'
-WHERE simple_preferences IS NULL;
